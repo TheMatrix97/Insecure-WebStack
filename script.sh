c83fbf5a5b0bb894b40ef8f948d32e67a52a7daa
Marc Catrisse <marc.catrisse@upc.edu>
2024-10-23 23:25:29 +0200

    add sed conf mysql

--- script.sh
+++ script.sh
@@ -44,6 +44,7 @@ echo "Done VSFTPD!"
 echo "Install MySQL"
 apt install -y mariadb-server
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';RENAME USER 'root'@'localhost' TO 'root'@'%';FLUSH PRIVILEGES;"
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 systemctl enable mysql --now
 echo "Done MySQL!"
 
